96fd15a8ee0fd5db1ad8dcbc42dcec74df008f31
Bob <0haizhu0@gmail.com>
2018-04-26 22:41:37 +0800

    2.1 简单的C程序示例

---
+++ chapter02/c2.1_A_Simple_Example_of_C/first.c
@@ -0,0 +1,17 @@
+/**
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2018/4/26
+ */
+#include <stdio.h>
+
+int main(void) {
+  int num;
+  num = 1;
+
+  printf("I am a simple ");
+  printf("computer.\n");
+  printf("My favorite number is %d because it is first.\n", num);
+
+  return 0;
+}
